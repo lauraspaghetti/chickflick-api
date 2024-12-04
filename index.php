d91fb4a0269e6db5f2117ef4e8625ce8fbc8db00
lauraspaghetti <laura.stassi17@gmail.com>
2024-12-04 14:46:18 +0100

    Update index.php
    Change in the keys of the URI array

--- index.php
+++ index.php
@@ -15,12 +15,12 @@ header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
 
-if ($parts[3] != "films") {
+if ($parts[1] != "films") {
     http_response_code(404);
     exit;
 }
 
-$id = $parts[4] ?? null;
+$id = $parts[2] ?? null;
 
 $config = require 'config.php';
 $database = new DataBase(
